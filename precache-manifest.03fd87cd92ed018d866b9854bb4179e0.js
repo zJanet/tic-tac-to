713e8a4587ea9364d2f20f4d95f1138ce004033e
zJanet <siwei@connect.hku.hk>
2019-06-17 13:39:06 +0800

    Updates

---
+++ precache-manifest.03fd87cd92ed018d866b9854bb4179e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a65bbe693e700f04afaec3935ee4c711",
+    "url": "/tic-tac-to/index.html"
+  },
+  {
+    "revision": "dd818a8e1c25bf19f321",
+    "url": "/tic-tac-to/static/css/main.0ebde3ef.chunk.css"
+  },
+  {
+    "revision": "685d0c597e8419129599",
+    "url": "/tic-tac-to/static/js/2.0e3f6a1a.chunk.js"
+  },
+  {
+    "revision": "dd818a8e1c25bf19f321",
+    "url": "/tic-tac-to/static/js/main.7c6e0642.chunk.js"
+  },
+  {
+    "revision": "726ba29b6a466f9d9392",
+    "url": "/tic-tac-to/static/js/runtime~main.b9d8b200.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/zJanet/tic-tac-to/precache-manifest.f8d8f47cda2ccf75403e8086cb642f23.js"
+  "/tic-tac-to/precache-manifest.03fd87cd92ed018d866b9854bb4179e0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/zJanet/tic-tac-to/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tic-tac-to/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
